410bb7eb6063a238475cef5cbb7903e74e17bf52
ARDA ÇALTEPE <a.caltepe2021@gtu.edu.tr>
2023-03-02 19:06:16 +0300

    Add project files.

---
+++ Highest Median/DataType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Highest_Median
+{
+    public class DataType
+    {
+        public string? country { get; set; }
+        public string? date { get; set; }
+        public int? daily_vaccinations { get; set; }
+        public string? vaccines { get; set; }
+    }
+}
---
+++ Highest Median/Program.cs
@@ -0,0 +1,82 @@
+﻿using Highest_Median;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+
+using (var reader = new StreamReader(@"C:\V\country_vaccination_stats.csv"))
+{
+    int i = 0;
+    List<DataType> listAll = new List<DataType>();
+    while (!reader.EndOfStream)
+    {
+
+        DataType vaccineData = new DataType();
+        var line = reader.ReadLine();
+        var values = line.Split(',');
+        if (i == 0)
+        {
+            i++;
+            continue;
+        }
+        else
+        {
+
+            vaccineData.country = values[0];
+            vaccineData.date = values[1];
+
+            if (values[2] == "")
+            {
+                values[2] = "0";
+                vaccineData.daily_vaccinations = Int32.Parse(values[2]);
+            }
+            else vaccineData.daily_vaccinations = Int32.Parse(values[2]);
+
+            vaccineData.vaccines = values[3];
+            listAll.Add(vaccineData);
+        }
+
+    }
+    FindTopThreeMedian(listAll);
+}
+
+void FindTopThreeMedian(List<DataType> listAll)
+{
+    List<string> CountryList = new List<string>();
+
+    foreach(var item in listAll)
+    {
+        if(!CountryList.Contains(item.country))
+        {
+            CountryList.Add(item.country);  
+        }
+    }
+
+    foreach(var item in CountryList)
+    {
+        double median = 0;
+        List<DataType> listing = new List<DataType>();
+        foreach (var item2 in listAll) 
+        {
+              
+            if (item == item2.country)
+            {
+                listing.Add(item2);
+            }
+        }
+        int counted = listing.Count;
+        int halfPart= counted / 2;
+        var sort=listing.OrderBy(x => x.daily_vaccinations);
+        if (counted % 2 == 0)
+        {
+            median=(Convert.ToDouble(((sort.ElementAt(halfPart).daily_vaccinations + sort.ElementAt(halfPart - 1).daily_vaccinations)) / 2));
+        }
+        else
+        {
+            median=Convert.ToDouble(sort.ElementAt((halfPart / 2) * 2).daily_vaccinations);
+        }
+        Console.WriteLine(item+"-"+median);
+        
+
+    }
+}
+
+ 
